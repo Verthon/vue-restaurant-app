a3c2b0cdc6a2f3d7af8ae58f0a42da9d7963dba6
Krzysztof Sordyl <krzysztof.sordyl@tsh.io>
2020-10-08 20:46:14 +0200

    feat: add modules

---
+++ src/store/modules/menu.ts
@@ -0,0 +1,21 @@
+import { MenuData } from '@/services/MenuSevice.types';
+import { State } from '../state';
+import MenuService from '@/services/MenuService';
+
+export const state: { menu: MenuData[] } = {
+  menu: []
+}
+
+export const mutations = {
+  SET_MENU(state: State, menu: MenuData[]) {
+    state.menu = menu
+  },
+}
+
+export const actions = {
+  async fetchMenu({ commit }) {
+    const response = await MenuService.getMenu()
+    commit('SET_MENU', response)
+    console.log(response);
+  }
+}
