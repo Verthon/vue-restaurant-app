00aa9c7e1910295d33e7d441468b559ee3e1ae23
Krzysztof Sordyl <krzysztof.sordyl@tsh.io>
2020-10-10 19:37:27 +0200

    feat: add Menu view

---
+++ src/components/Menu/MenuItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li class="menu__item">
+    <header class="menu__header">
+      <h3 class="menu__item__name">{{name}}</h3>
+      <span class="menu__item__price">{{price}}</span>
+    </header>
+    <p class="text menu__description">{{description}}</p>
+  </li>
+</template>
+
+<script>
+// import { formatPrice } from '@/utils/formatPrice'
+export default {
+  props: {
+    name: String,
+    price: String,
+    description: String
+  }
+  // computed: {
+  //   formattedPrice: formatPrice(this.price)
+  // }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Menu/MenuList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="menu__list">
+    <MenuItem v-for="item in category.data.data" :key="item.name" name="item.name" price="item.price" description="item.description" />
+      <!-- {category.data.data && category.data.data.map((item: MenuData) => <MenuItem key={item.name} menu={item} />)} -->
+    </ul>
+</template>
+
+<script>
+import MenuItem from '@/components/Menu/MenuItem'
+export default {
+  props: {
+    categories: Object
+  },
+  components: {
+    MenuItem
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
