7dc81d5a808d933dfba542f80664c2e11aed20a2
Verthon <christopher.sordyl@gmail.com>
2021-10-06 19:21:58 +0200

    feat: add vue-testing-library

--- tests/unit/Button.spec.js
+++ tests/unit/Button.spec.js
@@ -1,16 +1,15 @@
-import flushPromises from 'flush-promises'
-import { mount } from '@vue/test-utils'
+import { render } from '@testing-library/vue'
 
 import Button from '@/components/Button/Button.vue'
 
 const createWrapper = () => {
-  return mount(Button)
+  return render(Button)
 }
 
 describe('Button', () => {
   it('should render button correctly ', async () => {
     const wrapper = createWrapper()
-    await flushPromises()
-    expect(wrapper.element.className).toBe('btn')
+    wrapper.debug()
+    expect(wrapper.container.className).toBe('btn')
   })
 })
